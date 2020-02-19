2daae8610f7961a9a37d50f89b1442b83895f686
Serge Miliuta <siarhei_miliuta@senla.eu>
2020-02-19 18:00:40 +0300

    refactoring and comments

--- src/main/java/com/vaiss/edl/Main.java
+++ src/main/java/com/vaiss/edl/Main.java
@@ -8,47 +8,59 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.*;
 
+/**
+ * The main purpose of that app is to benchmark LinkedList vs ArrayList add(int index, E element) method
+ * As a result you'll got to .csv files with two columns.
+ * First one contains indexes that have been used to insert test data, second one - time elapsed to make insertion (in nanoseconds)
+ * It is highly recommended to follow some rules while changing constants:
+ * SIZE % INDEX_SCOPE must be 0
+ * INDEX_SCOPE % INDEX_STEP must be 0
+ */
 
 public class Main {
-    public static final Integer CAPACITY = 5_000_000;
-    public static final String CSV_FILE_NAME = "D:/";
-    public static volatile List<String> resultList = new CopyOnWriteArrayList<>();
+    //test collection size
+    static final Integer SIZE = 500000;
+    //a range of indexes to be processed by each thread
+    static final Integer INDEX_SCOPE = 100;
+    //index change step. E.g. if INDEX_STEP is 20 insertion will be made and benchmarked for indexes 0, 20, 40, 60 etc.
+    static final Integer INDEX_STEP = 20;
+    //amount of working threads
+    static final Integer THREADS_NUM = 20;
+    //number of elements to be inserted into test collection
+    static final Integer INSERT_AMOUNT = 100;
+    static final String CSV_FILE_PATH = "D:/";
 
     public static void main(String[] args) throws InterruptedException, FileNotFoundException {
-        String linkedListName = LinkedList.class.getName();
-        String arrayListName = ArrayList.class.getName();
         System.out.println("Processing LinkedList");
-        execute(linkedListName);
+        execute("LinkedList");
         System.out.println("Processing ArrayList");
-        execute(arrayListName);
+        execute("ArrayList");
     }
 
     static void execute(String type) throws InterruptedException, FileNotFoundException {
         List<Callable<List<String>>> callables = new ArrayList<>();
-        for (int i = 0; i < CAPACITY; i += 100) {
-            callables.add(new Task(type, i, i + 100));
+        for (int i = 0; i < SIZE; i += INDEX_SCOPE) {
+            callables.add(new Task(type, i, i + INDEX_SCOPE));
         }
 
-        ExecutorService pool = Executors.newFixedThreadPool(20);
+        ExecutorService pool = Executors.newFixedThreadPool(THREADS_NUM);
         List<Future<List<String>>> readyFutures = pool.invokeAll(callables);
         pool.shutdown();
 
         List<String> resultList = new ArrayList<>();
 
-        readyFutures.stream().forEach((future) -> {
+        readyFutures.forEach((future) -> {
             try {
                 resultList.addAll(future.get());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } catch (ExecutionException e) {
-                e.printStackTrace();
+            } catch (InterruptedException | ExecutionException e) {
+                System.out.println(e.getMessage());
             }
         });
         printCSV(resultList, type);
     }
 
     static void printCSV(List<String> list, String name) throws FileNotFoundException {
-        File csvFile = new File(CSV_FILE_NAME + name + CAPACITY + ".csv");
+        File csvFile = new File(CSV_FILE_PATH + name + SIZE + ".csv");
         try (PrintWriter pw = new PrintWriter(csvFile)) {
             list.forEach(pw::println);
         }
@@ -56,34 +68,39 @@ public class Main {
 
     static List<String> check(String listType, int startIndex, int endIndex) throws ClassNotFoundException {
         List<String> resultsList = new ArrayList<>();
-        List<Integer> list;
-        if (listType.contains("LinkedList")) {
-            list = new LinkedList<>();
-        } else if (listType.contains("ArrayList")) {
-            list = new ArrayList<>(CAPACITY);
-        } else {
-            throw new ClassNotFoundException("Class must be LinkedList or ArrayList");
-        }
+        List<Integer> list = getList(listType);
 
-        for (int index = startIndex; index < endIndex; index += 20) {
+        for (int index = startIndex; index < endIndex; index += INDEX_STEP) {
 
             System.out.println(index);
 
-            for (int i = 0; i < CAPACITY; i++) {
-                list.add(new Integer(i));
+            for (int i = 0; i < SIZE; i++) {
+                list.add(i);
             }
 
             long start = System.nanoTime();
-            for (int i = 0; i < 100; i++) {
-                list.add(index, new Integer(Integer.MAX_VALUE));
+            for (int i = 0; i < INSERT_AMOUNT; i++) {
+                list.add(index, Integer.MAX_VALUE);
             }
             long end = System.nanoTime() - start;
 
-            resultsList.add(String.join(",", list.getClass().getName(), index +"", end+""));
+            resultsList.add(String.join(",", index + "", end + ""));
             list.clear();
         }
 
         return resultsList;
+    }
+
+    static List<Integer> getList(String listType) throws ClassNotFoundException {
+        List<Integer> list;
+        if ("LinkedList".equals(listType)) {
+            list = new LinkedList<>();
+        } else if ("ArrayList".equals(listType)) {
+            list = new ArrayList<>(SIZE);
+        } else {
+            throw new ClassNotFoundException("Class must be LinkedList or ArrayList");
+        }
+        return list;
     }
 }
 
@@ -92,7 +109,7 @@ class Task implements Callable<List<String>> {
     private int start;
     private int end;
 
-    public Task(String className, int start, int end) {
+    Task(String className, int start, int end) {
         this.className = className;
         this.start = start;
         this.end = end;
